25f2f330a6b5390f0a487d35521b30dec9b21316
Shakeel Mohamed <contact@shakeel.xyz>
2021-08-27 22:28:07 -0700

    switch homepage to pug

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,13 @@ const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
 module.exports = function(eleventyConfig) {
     // File extensions to passthrough copy
     eleventyConfig.setTemplateFormats([
+        "pug",
         "png",
         "jpg",
         "js",
         "css"
       ]);
+    // Flags from CLI usage --formats=html,md,png,jpg,pug
 
     // Inclusive language, TODO: how to use this
     eleventyConfig.addPlugin(inclusiveLangPlugin);
@@ -21,4 +23,12 @@ module.exports = function(eleventyConfig) {
     };
     let markdownLib = markdownIt(options);
     eleventyConfig.setLibrary("md", markdownLib);
+
+    
+
+    // Pug specific options 
+    eleventyConfig.setPugOptions({
+        debug: true,
+        pretty: "    "
+    });
 };
