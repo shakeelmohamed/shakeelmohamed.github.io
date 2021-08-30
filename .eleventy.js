417072461a4591b50f8b2b47a7fd14ded897c55d
Shakeel Mohamed <contact@shakeel.xyz>
2021-08-30 00:12:09 -0700

    fix reverse sort on recent posts

--- .eleventy.js
+++ .eleventy.js
@@ -32,5 +32,13 @@ module.exports = function(eleventyConfig) {
     // Template aliases
     eleventyConfig.addLayoutAlias('post', 'layouts/post.pug');
 
+    // TODO: reverse sort order for blog posts, newest first
+    // Use the default sorting algorithm in reverse (descending dir, date, filename)
+    // Note that using a template engine’s `reverse` filter might be easier here
+    eleventyConfig.addCollection("post", function(collectionApi) {
+        console.dir(collectionApi);
+        return collectionApi.getFilteredByTag("post").reverse();
+    });
+
     // TODO: integrate alex for biased & bad word checks https://github.com/get-alex/alex
 };
