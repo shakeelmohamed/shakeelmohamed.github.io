9307e57afec679a78986ec05ea693fb2573f306a
Shakeel Mohamed <contact@shakeel.xyz>
2021-09-14 21:44:37 -0700

    no idea what I'm doing

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,4 +1,4 @@
-var isLocal = location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "192.168.0.127";
+var isLocal = location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "192.168.0.127:8080";
 if (!isLocal && window.location.protocol === "http:") {
     window.location.protocol = "https:";
 }
