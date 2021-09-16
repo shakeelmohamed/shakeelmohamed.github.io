12bc638b90cef9e9cb4fe1a4f559cae07b347764
Shakeel Mohamed <iam@shakeel.xyz>
2021-09-15 18:24:55 -0700

    temp, comment out the https redirect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
-var isLocal = location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "192.168.0.127:8080";
-if (!isLocal && window.location.protocol === "http:") {
-    window.location.protocol = "https:";
-}
+// var isLocal = location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "192.168.0.127:8080";
+// if (!isLocal && window.location.protocol === "http:") {
+//     window.location.protocol = "https:";
+// }
 
 var num = Math.floor(Math.random() * 5) + 1;
 function getRandomLogo(size) {
