e6bb556d40b7a9938233547f07ae456163a881fb
Shakeel Mohamed <contact@shakeel.xyz>
2026-03-14 19:36:43 -0700

    add mobile compare test script

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
     plugins: [
         require('postcss-import'),
         require('autoprefixer'),
-        isProduction && require('cssnano')({
+        require('cssnano')({
             preset: 'default'
         })
-    ].filter(Boolean)
+    ]
 };
